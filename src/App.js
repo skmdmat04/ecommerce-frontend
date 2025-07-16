4cf9408e3a28da85625130214df05266e34a213a
skmdmat04 <86888947+skmdmat04@users.noreply.github.com>
2025-07-16 18:50:53 +0530

    Update App.js hellow from uat

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Container } from './components/StyledComponents';
 const App = () => {
   return (
     <Container>
-<h1>hello uat</h1>
+<h1>hello from uat</h1>
       <Header />
       <Shop />
       <Footer />
