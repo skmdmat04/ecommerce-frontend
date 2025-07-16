6afc114baac5682a91378546b387db0becd76aad
skmdmat04 <86888947+skmdmat04@users.noreply.github.com>
2025-07-16 21:16:34 +0530

    Update App.js 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Container } from './components/StyledComponents';
 const App = () => {
   return (
     <Container>
-<h1>hello from uat1</h1>
+<h1>hello from uat2</h1>
       <Header />
       <Shop />
       <Footer />
